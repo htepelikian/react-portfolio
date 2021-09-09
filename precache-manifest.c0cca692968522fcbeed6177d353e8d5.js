a161497d26449f9b0ea5821357d2207b36f42a8c
Hripsime Tepelikian <htepelikian@gmail.com>
2021-09-09 15:02:35 -0700

    Updates

---
+++ precache-manifest.c0cca692968522fcbeed6177d353e8d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "172b55f570e404259ceb1459ce86eeda",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "9121a7df431a7201f2c7",
+    "url": "/react-portfolio/static/css/2.fa6c921b.chunk.css"
+  },
+  {
+    "revision": "f75aaa874535589da764",
+    "url": "/react-portfolio/static/css/main.a20fa0a3.chunk.css"
+  },
+  {
+    "revision": "9121a7df431a7201f2c7",
+    "url": "/react-portfolio/static/js/2.0772de4b.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/react-portfolio/static/js/2.0772de4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f75aaa874535589da764",
+    "url": "/react-portfolio/static/js/main.80da8032.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio/About/precache-manifest.07e839a0e1705005235ad9b9e9886534.js"
+  "/react-portfolio/precache-manifest.c0cca692968522fcbeed6177d353e8d5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-portfolio/About/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
