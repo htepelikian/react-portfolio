6f94755cd0739b93b2843c5e0d096bac41b298f5
Hripsime Tepelikian <htepelikian@gmail.com>
2021-09-09 14:59:47 -0700

    Updates

---
+++ precache-manifest.07e839a0e1705005235ad9b9e9886534.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f8ec422d83c83e28f31935c9308426a",
+    "url": "/react-portfolio/About/index.html"
+  },
+  {
+    "revision": "9121a7df431a7201f2c7",
+    "url": "/react-portfolio/About/static/css/2.fa6c921b.chunk.css"
+  },
+  {
+    "revision": "d9d1f0eb5f00ddafef4d",
+    "url": "/react-portfolio/About/static/css/main.a20fa0a3.chunk.css"
+  },
+  {
+    "revision": "9121a7df431a7201f2c7",
+    "url": "/react-portfolio/About/static/js/2.0772de4b.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/react-portfolio/About/static/js/2.0772de4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9d1f0eb5f00ddafef4d",
+    "url": "/react-portfolio/About/static/js/main.cd664ad0.chunk.js"
+  },
+  {
+    "revision": "9167bd33ff01d58f5bae",
+    "url": "/react-portfolio/About/static/js/runtime-main.0b9785cb.js"
+  }
+]);
